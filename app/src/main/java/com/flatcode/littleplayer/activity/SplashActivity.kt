e42f553418b8c5079adeb8b03b4bdb34c9da1513
selimdawa <selimdawa@gmail.com>
2026-07-26 16:47:30 +0300

    Splash Theme - Fix

--- app/src/main/java/com/flatcode/littleplayer/activity/SplashActivity.kt
+++ app/src/main/java/com/flatcode/littleplayer/activity/SplashActivity.kt
@@ -1,28 +1,36 @@
 package com.flatcode.littleplayer.activity
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import com.flatcode.littleplayer.databinding.ActivitySplashBinding
 import com.flatcode.littleplayer.utils.launchActivity
 import com.flatcode.littleplayer.viewmodel.MusicViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.milliseconds
 
-@SuppressLint("CustomSplashScreen")
 @AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivitySplashBinding
     private val context: Context = this@SplashActivity
     private val viewModel: MusicViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
         super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel.loadAudioData()
-        context.launchActivity<MainActivity>()
-        finish()
+
+        lifecycleScope.launch {
+            delay(500.milliseconds)
+            context.launchActivity<MainActivity>()
+            finish()
+        }
     }
 }
