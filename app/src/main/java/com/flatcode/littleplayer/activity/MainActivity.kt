45eb69aaf333f22cdc08e5bb842fbf9e7f34bc35
selimdawa <selimdawa@gmail.com>
2026-08-03 02:56:51 +0300

    Clean Code

--- app/src/main/java/com/flatcode/littleplayer/activity/MainActivity.kt
+++ app/src/main/java/com/flatcode/littleplayer/activity/MainActivity.kt
@@ -8,7 +8,6 @@ import android.os.Build
 import android.os.Bundle
 import android.text.TextUtils.TruncateAt
 import android.view.Gravity
-import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup.MarginLayoutParams
 import android.view.animation.AnimationUtils
