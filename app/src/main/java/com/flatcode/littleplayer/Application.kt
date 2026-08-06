ad6c44f19b8f3d9845e5ff29e4daf973a73d1d73
selimdawa <selimdawa@gmail.com>
2026-08-07 01:13:22 +0300

    Dark Mode - Test

--- app/src/main/java/com/flatcode/littleplayer/Application.kt
+++ app/src/main/java/com/flatcode/littleplayer/Application.kt
@@ -10,12 +10,30 @@ import coil.disk.DiskCache
 import coil.memory.MemoryCache
 import dagger.hilt.android.HiltAndroidApp
 import io.selimdawa.multicolors.MultiColorManager
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.intPreferencesKey
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltAndroidApp
 class Application : Application(), ImageLoaderFactory {
 
+    @Inject
+    lateinit var dataStore: DataStore<Preferences>
+
     override fun onCreate() {
         super.onCreate()
+
+        MainScope().launch {
+            val darkModeKey = intPreferencesKey("dark_mode_preference")
+            val mode = dataStore.data.map { it[darkModeKey] ?: AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM }.first()
+            AppCompatDelegate.setDefaultNightMode(mode)
+        }
 
         MultiColorManager.init(this)
 
