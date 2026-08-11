7186c831d469f4138657b67df7e0cde50f6e8c66
selimdawa <selimdawa@gmail.com>
2026-08-11 07:42:32 +0300

    Clean Code

--- app/src/main/java/com/flatcode/littleplayer/viewmodel/EqualizerViewModel.kt
+++ app/src/main/java/com/flatcode/littleplayer/viewmodel/EqualizerViewModel.kt
@@ -3,7 +3,7 @@ package com.flatcode.littleplayer.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.flatcode.littleplayer.data.entity.EqualizerEntity
-import com.flatcode.littleplayer.repository.MusicRepository
+import com.flatcode.littleplayer.repository.MusicRoomRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -13,7 +13,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class EqualizerViewModel @Inject constructor(
-    private val repository: MusicRepository
+    private val repository: MusicRoomRepository
 ) : ViewModel() {
 
     private val _equalizerSettings = MutableStateFlow<EqualizerEntity?>(null)
